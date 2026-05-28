7c25a1390d75707614ed4fcce9429aae06b222ac
tomwlee904-sys <tomwlee904@example.com>
2026-05-28 23:45:53 +0100

    Remove Next.js project files - restore to pure static HTML site

--- src/components/Divider.tsx
+++
@@ -1,7 +0,0 @@
-export default function Divider({ className = "" }: { className?: string }) {
-  return (
-    <div
-      className={`border-t border-gray-100 dark:border-[#2a2a3e] my-8 ${className}`}
-    />
-  );
-}
