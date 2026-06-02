f99344e3b3b9fd1f3bcfaa9d7343c68183ed0655
tomwlee904-sys <tomwlee904@gmail.com>
2026-06-02 11:46:35 +0100

    Fix blank pages: add React mount point to root index.html
    - Modified root index.html to include <div id='root'> wrapping body
      content and React bundle script tag
    - Build script now does vite build && cp ../index.html dist/index.html
      so the SPA shell has all CSS/styles from the root
    - Removed global.css import (caused conflicts)
    - React app mounts inside #root and replaces homepage content
      with the correct page component

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/variables.css'
-import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
